147234fee143d99da51794f2803f7472538d09f2
GodSlappedMe <narampark92@gmail.com>
2023-10-09 17:39:53 -0400

    final results page

---
+++ assets/js/finalresults.js
@@ -0,0 +1,33 @@
+const selectedRecipeData = JSON.parse(localStorage.getItem("Selected Recipe"));
+
+if (selectedRecipeData) {
+  const recipeCardImage = document.getElementById("recipe-image");
+  const recipeCardTitle = document.getElementById("recipe-title");
+  const recipeCardLink = document.getElementById("recipe-link");
+  const recipeCardSummary = document.getElementById("recipe-summary");
+
+  recipeCardImage.src = selectedRecipeData[0].image;
+  recipeCardTitle.textContent = selectedRecipeData[0].title;
+  recipeCardLink.href = selectedRecipeData[0].sourceUrl;
+  recipeCardSummary.textContent = selectedRecipeData[0].summary;
+} else {
+  console.log("boop");
+}
+
+const selectedWorkoutData = localStorage.getItem("Duration Minutes");
+
+if (selectedWorkoutData) {
+  const durationMinutes = document.getElementById("duration-workout");
+
+  durationMinutes.textContent = `Duration Minutes: ${selectedWorkoutData} Minutes`;
+}
+
+const selectedCaloriesData = localStorage.getItem(
+  "Calories Burned Calculation"
+);
+
+if (selectedCaloriesData) {
+  const caloriesBurned = document.getElementById("calories-burned-calculation");
+
+  caloriesBurned.textContent = `Calories Burned: ${selectedCaloriesData}`;
+}
